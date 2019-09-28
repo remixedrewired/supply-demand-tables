f664845ebedc06b20b3433f5e1abf0091ced650d
Anton Shapovalov <shapikcool@gmail.com>
2019-09-28 17:55:43 +0800

    added separeted styles module;

---
+++ src/styles/selectInput.js
@@ -0,0 +1,11 @@
+export default (theme) => ({
+  formControl: {
+    [theme.breakpoints.down("xs")]: {
+      marginRight: "8%",
+      minWidth: 165,
+    },
+    [theme.breakpoints.up("sm")]: {
+      marginRight: "20%",
+      minWidth: 225,
+    },
+  }
---
+++ src/styles/simpleSelectInput.js
@@ -0,0 +1,10 @@
+export default (theme) => ({
+  select: {
+    width: "75%",
+    position: "absolute",
+    top: "7px",
+  },
+  fontSize: {
+    fontSize: "14px",
+  },
+});
